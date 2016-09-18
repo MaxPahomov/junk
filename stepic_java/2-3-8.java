9de6b29ff04eff50213f52635c12ea3b33fd9f62
Vladislav Gorevoy <gorevoy-vladislav@users.noreply.github.com>
2016-09-18 21:21:24 +0700

    Update 2-3-8.java

--- stepic_java/2-3-8.java
+++ stepic_java/2-3-8.java
@@ -3,6 +3,6 @@ public class Main {
         String a1 = "A" + ('\t' + '\u0003'); //A12
         String a2 = 'A' + '1' + "2"; // 1142
         String a3 = 'A' + "12"; //A12
-        String a4 = "A" + 12; //12
+        String a4 = "A" + 12; //A12
     }
 }
